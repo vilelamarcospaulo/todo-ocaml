b8370fd923cc4e121bd248517b61cb65c61c4d30
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2024-04-13 18:18:25 -0300

    setup Dream web server

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let sql_uri = "sqlite3:db.sqlite"
+
+let _ =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.sql_pool sql_uri
+  @@ Dream.sql_sessions
+  @@ Dream.router [
+    Dream.get "/" (fun _ -> Dream.html "Hello, world!");
+  ]
+
