974241b6247c219f7ae5cfd2931d5c3fcc454ccb
marcos vilela <marcospaulo.vilelasantos@gmail.com>
2024-04-20 16:53:19 -0300

    feat: test flow include body parser

--- test/test_todo.ml
+++ test/test_todo.ml
@@ -4,11 +4,12 @@ let connection_string = "sqlite3:db_test.sqlite"
 let dream_runner = Dream.test @@ Dream.sql_pool connection_string Todo.Http.routes
 
 module BddTestFlow = struct
-  type response = { status : int; body : string }
+  type 'a parsed = 'a option
+  type 'a response = { status : int; body : string; parsed_body : 'a parsed }
 
-  let bind (response : response) (f : response -> response) = f response
+  let bind (response : 'a response) (f : 'a response -> 'a response) = f response
 
-  let finish (response : response) (f : response -> response) =
+  let finish (response : 'a response) (f : 'a response -> 'a response) =
     let _result = f response in
     ()
 
@@ -20,24 +21,35 @@ module BddTestFlow = struct
     let received_status = result |> Dream.status |> Dream_pure.Status.status_to_int in
     let received_body = Lwt_main.run @@ Dream.body result in
 
-    { status = received_status; body = received_body }
+    { status = received_status; body = received_body; parsed_body = None }
 
-  let then_the_status_should_be (expected : int) (response : response) =
+  let then_the_status_should_be expected response =
     Alcotest.(check int) "Check the status" expected response.status;
     response
 
-  let then_the_body_should_be (expected : string) (response : response) =
+  let then_the_body_should_be expected response =
     Alcotest.(check string) "Check response body" expected response.body;
     response
 
-  let then_db_should_have_n expected (module Db : DB) query_count (response : response) =
+  let then_with_parsed_body ty_of_yojson response =
+    let parsed = response.body |> Yojson.Safe.from_string |> ty_of_yojson in
+
+    { response with parsed_body = Some parsed }
+
+  let then_body_should_apply_to (f : 'a -> bool) response =
+    let parsed_body = Option.get response.parsed_body in
+    Alcotest.(check bool) "Check the parsed body" true (f parsed_body);
+
+    response
+
+  let then_db_should_have_n expected (module Db : DB) query_count response =
     let db_count = Lwt_main.run @@ Db.find query_count () in
     let count = match db_count with Ok count -> count | _ -> Alcotest.failf "Error" in
 
     Alcotest.(check int) "Check the number of items by db_query" expected count;
     response
 
-  let then_db_should_not_have_any_items (module Db : DB) query_count (response : response) =
+  let then_db_should_not_have_any_items (module Db : DB) query_count response =
     then_db_should_have_n 0 (module Db) query_count response
 end
 
@@ -67,6 +79,8 @@ let test_create_todo_successfully (module Db : DB) () =
   >!> then_the_status_should_be 200
   >!> then_the_body_should_be
         "{\"id\":1,\"title\":\"foo\",\"description\":\"bar\",\"completedAt\":null}"
+  >!> then_with_parsed_body Todo.Item.t_of_yojson
+  >!> then_body_should_apply_to (fun item -> item.Todo.Item.id = 1)
   >! then_db_should_have_n 1 (module Db) Todo.Item.Q.all
 
 let _ =
